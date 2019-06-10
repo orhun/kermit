e8c2607737eacd172ea3144d11aaf8d07f9b2eef
KeyLo99 <keylo99official@gmail.com>
2019-06-10 20:59:25 +0300

    Spawn terminal async

--- k3rmit.c
+++ k3rmit.c
@@ -5,31 +5,32 @@
 
 int main(int argc, char *argv[]) {
     GtkWidget *window, *terminal;
-
     /* Initialize GTK, the window and the terminal */
     gtk_init(&argc, &argv);
     terminal = vte_terminal_new();
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "myterm");
-
     /* Start a new shell */
     gchar **envp = g_get_environ();
     gchar **command = (gchar *[]){g_strdup(g_environ_getenv(envp, "SHELL")), NULL };
     g_strfreev(envp);
-    vte_terminal_spawn_sync(VTE_TERMINAL(terminal),
-        VTE_PTY_DEFAULT,
-        NULL,       /* working directory  */
-        command,    /* command */
-        NULL,       /* environment */
-        0,          /* spawn flags */
-        NULL, NULL, /* child setup */
-        NULL,       /* child pid */
-        NULL, NULL);
-
+    /* Spawn asynchronous terminal */
+    vte_terminal_spawn_async(VTE_TERMINAL(terminal), 
+        VTE_PTY_DEFAULT, 
+        NULL,
+        command, 
+        NULL, 
+        G_SPAWN_DO_NOT_REAP_CHILD,
+        NULL,
+        NULL,
+        NULL,
+        -1,
+        NULL,
+        NULL,
+        NULL);
     /* Connect signals */
     g_signal_connect(window, "delete-event", gtk_main_quit, NULL);
     g_signal_connect(terminal, "child-exited", gtk_main_quit, NULL);
-
     /* Put widgets together and run the main loop */
     gtk_container_add(GTK_CONTAINER(window), terminal);
     gtk_widget_show_all(window);
