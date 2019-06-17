4668eccad8efbd6931fda04f33ddb64dc137dcc7
KeyLo99 <keylo99official@gmail.com>
2019-06-17 04:24:30 +0300

    Shebang header added to source files

--- k3rmit.c
+++ k3rmit.c
@@ -1,4 +1,20 @@
-// gcc -O2 -Wall $(pkg-config --cflags vte-2.91) k3rmit.c -o k3rmit.o $(pkg-config --libs vte-2.91)
+/*
+ * k3rmit, Terminal emulator that aims to be simple, fast and effective.
+ * Copyright (C) 2019 by KeyLo99 <https://www.github.com/KeyLo99>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "k3rmit.h"
 #include <stdio.h>
