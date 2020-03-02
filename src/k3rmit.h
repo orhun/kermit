f66b8636094a653197b287ad4edee9ee13ceb7ae
orhun <orhunparmaksiz@gmail.com>
2020-03-02 21:26:39 +0300

    Update version variables for release 2.3

--- src/k3rmit.h
+++ src/k3rmit.h
@@ -18,7 +18,7 @@
 
 #include <vte/vte.h>
 #define TERM_NAME "k3rmit"
-#define TERM_VERSION "2.2"
+#define TERM_VERSION "2.3"
 #define TERM_FONT "Monospace"
 #define TERM_FONT_DEFAULT_SIZE 9
 #define TERM_LOCALE "en_US.UTF-8"
