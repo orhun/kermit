a8054fcf5086f31d6c8bef6c443c1a966703e15f
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-02-05 18:31:44 +0300

    Bump the version

--- src/kermit.h
+++ src/kermit.h
@@ -17,7 +17,7 @@
 
 #include <vte/vte.h>
 #define TERM_NAME "kermit"
-#define TERM_VERSION "3.6"
+#define TERM_VERSION "3.7"
 #define TERM_FONT "Monospace"
 #define TERM_FONT_DEFAULT_SIZE 9
 #define TERM_LOCALE "en_US.UTF-8"
